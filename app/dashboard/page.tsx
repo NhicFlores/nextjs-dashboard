d2a501d91dc14bee76f0631da6c2fcc6c3436b03
NhicFlores <nflores@mankowindows.com>
2024-04-16 12:24:07 -0500

    completed ch 7 data fetching, used the cardwrapper instead of card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,38 @@
-//dashboard page 
+//dashboard page
 
-export default function Page() {
-    return <p>Dashboard Page</p>
+import { lusitana } from "../ui/fonts";
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards'
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../lib/data";
+
+//this page is async so we can use await to fetch data 
+
+export default async function Page() {
+    const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
+    const cardData = await fetchCardData();
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2x1`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <CardWrapper></CardWrapper>
+                {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+                {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+                {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+                {/* <Card
+                    title="Total Customers"
+                    value={numberOfCustomers}
+                    type="customers"
+                /> */}
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <RevenueChart revenue={revenue}/>
+                <LatestInvoices latestInvoices={latestInvoices}/>
+            </div>
+        </main>
+    );
 }
