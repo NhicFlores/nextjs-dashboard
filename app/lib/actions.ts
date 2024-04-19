2fbfccbb188ad1ebcde939635222ce65ed7c072a
NhicFlores <nflores@mankowindows.com>
2024-04-19 12:36:15 -0500

    ch 12 added edit invoice routing and actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -56,3 +56,24 @@ export async function createInvoice(formData: FormData) {
     console.log("path revalidated");
     redirect('/dashboard/invoices');
 }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true});
+
+export async function updateInvoice(id: string, formData: FormData){
+    const { customer_id, amount, status } = UpdateInvoice.parse({
+        customer_id: formData.get('customer_id'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customer_id}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices/');
+    redirect('/dashboard/invoices/');
+}
